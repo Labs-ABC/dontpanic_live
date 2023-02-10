52150cd804bfe176fff58ba9adfd3ee35d601b56
Alice Akaki <akakialice@gmail.com>
2023-02-10 15:38:54 -0300

    Teste Validate Equation Result True

--- DontPanicBabyTest/UnitTest1.cs
+++ DontPanicBabyTest/UnitTest1.cs
@@ -1,16 +1,54 @@
+using DontPanicBabyApi.Models;
+using DontPanicBabyApi.Services;
+
 namespace DontPanicBabyTest
 {
   public class Tests
   {
+        public EquationInput correct, incorrect, invalid;
+        public EquationService service;
+
     [SetUp]
     public void Setup()
     {
-    }
+            correct = new EquationInput
+            {
+                FirstInput = '2',
+                SecondInput = '1',
+                ThirdInput = '*',
+                FourthInput = '2',
+                FifthInput = '-',
+                SixthInput = '0'
+            };
+
+            incorrect = new EquationInput
+            {
+                FirstInput = '2',
+                SecondInput = '1',
+                ThirdInput = '*',
+                FourthInput = '2',
+                FifthInput = '+',
+                SixthInput = '0'
+            };
+
+            invalid = new EquationInput
+            {
+                FirstInput = '2',
+                SecondInput = '1',
+                ThirdInput = '+',
+                FourthInput = '2',
+                FifthInput = '-',
+                SixthInput = '0'
+            };
+
+            service = new EquationService();
+        }
 
     [Test]
-    public void Test1()
+    public void Test_Validate_Equation_Result_Return_True()
     {
-      Assert.Pass();
+      var result = service.ValidateEquationResult(correct);
+      Assert.That(result, Is.True);
     }
   }
 }
