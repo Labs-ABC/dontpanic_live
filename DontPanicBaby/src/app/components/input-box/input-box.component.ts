771bbd702a1cbad8d91db4fe1e61b51e24bf51ad
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2023-02-13 18:11:49 -0300

    Adiciona pulo pro próximo input automático

--- DontPanicBaby/src/app/components/input-box/input-box.component.ts
+++ DontPanicBaby/src/app/components/input-box/input-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-input-box',
@@ -14,15 +14,43 @@ export class InputBoxComponent {
 
   @Output()
   valueChange = new EventEmitter<string>()
-  
+
+  moveFocus(input: HTMLInputElement) {
+    if (input.value.length === 1) {
+      const nextInputId = Number(input.parentElement?.id) + 1;
+      const nextInput = window.document.getElementById(nextInputId.toString());
+      if (nextInput) {
+        nextInput.querySelector('input')?.focus();
+      }
+    }
+  }
+
+  handleBackspace(event: KeyboardEvent, input: HTMLInputElement) {
+    if (event.key == 'Backspace') {
+      if (input.value.length === 1) {
+        this.value = '';
+        return;
+      }
+      const previousInputId = Number(input.parentElement?.id) - 1;
+      const previousInput = window.document.getElementById(previousInputId.toString());
+      if (previousInput) {
+        const childInput = previousInput.querySelector('input');
+        if (childInput) {
+          childInput.focus();
+          childInput.value = '';
+        }
+      }
+    }
+  }
+
   private operators = '+-*/'
 
-  constructor(private el: ElementRef){
-    this.value = ''
+  constructor() {
+    this.value = '';
   }
 
   @HostListener('keypress', ['$event']) onKeyPress(event: KeyboardEvent){
-    const char = String.fromCharCode(event.charCode);
+    const char = String.fromCharCode(event.keyCode);
     if (!this.isValid(char)){
       event.preventDefault();
     }
