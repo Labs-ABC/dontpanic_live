0b790396c8077ce961d28e4257574ff80a6bb4c5
gsilva-v <gaa.silva53@gmail.com>
2023-02-15 17:20:18 -0300

    Feature: Dockerfile Backend

--- DontPanicBabyApi/Program.cs
+++ DontPanicBabyApi/Program.cs
@@ -14,10 +14,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 app.UseCors();
 
-
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()){
   app.UseSwagger();
   app.UseSwaggerUI();
 }
