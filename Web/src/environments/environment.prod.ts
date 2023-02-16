e438b16affc1e1a8fa017f8f4722c8e69b0c77ac
gsilva-v <gaa.silva53@gmail.com>
2023-02-16 17:40:37 -0300

    Docker-compose funcionando

--- Web/src/environments/environment.prod.ts
+++ Web/src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
 export const environment = {
   production: true,
-  url: 'http://labs-abc.42sp.org.br:5014',
+  url: 'http://localhost:5000',
   EquationEndPoint: '/api/Equation'
 }
+
+// export const environment = {
+//   production: true,
+//   url: 'http://labs-abc.42sp.org.br:5014',
+//   EquationEndPoint: '/api/Equation'
+// }
